2b254e24bb366ec43e202719498066d3afe8b115
hbao-newbie <baob1906432@student.ctu.edu.vn>
2022-11-01 22:09:56 +0700

    merge sort

--- src/components/MergeSort.vue
+++ src/components/MergeSort.vue
@@ -1,12 +1,31 @@
 <template>
     <div class="element" style="height: 250px;"></div>
+    <div
+        class="element"
+        v-for="(element, index) in array" :key="index"
+        :style="{
+            transform: `translate(${element.x}px, ${element.y}px)`,
+        }"
+    >
+        <div
+            class="element-item"
+            :style="{
+                backgroundColor: `${element.color}`,
+                height: `${element.data * 10}px`,
+            }"
+        >
+        </div>
+        <div class="element-item-number">{{ element.data }}</div>
+    </div>
+    <div class="main-element"></div>
+    <div class="main-element">
         <div
             class="element"
-            v-for="(element, index) in array" :key="index"
+            v-for="(element, index) in arrayTemp" :key="index"
             :style="{
                 transform: `translate(${element.x}px, ${element.y}px)`,
             }"
-        >
+            >
             <div
                 class="element-item"
                 :style="{
@@ -15,19 +34,33 @@
                 }"
             >
             </div>
-        <div class="element-item-number">{{ element.data }}</div>
+            <div class="element-item-number">{{ element.data }}</div>
+        </div>
     </div>
 </template>
 
 <script>
-    import mergesort from '../utils/mergesort'
+    import mergesort from '../utils/mergesort';
 
     export default {
         props: ['array', 'colors'],
+        data() {
+            return {
+                arrayTemp: [],
+            }
+        },
         methods: {
             async start() {
-               mergesort(this.array.length, this.array, this.colors);
+            //    mergesort(this.array.length, this.array, this.colors);
+                mergesort(this.array.length, this.array, this.colors, this.arrayTemp);
             },
-        }
+        },
     }
 </script>
+
+<style scoped>
+    .main-element {
+        margin-top: 30px;
+        height: 220px;
+    }
+</style>
